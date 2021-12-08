36d319034608e89ca9a5a162248ab8daf3762cac
thepolkamonster <72528293+thepolkamonster@users.noreply.github.com>
2021-12-08 08:11:46 +0530

    Add files via upload

---
+++ BFS.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    list<int> list1;
+    list<int> list2(7);
+    list1.push_back(5);
+    list1.push_back(7);
+    list<int> :: iterator t;
+    t=list1.begin();
+    cout<<*iter;
+
+
+}
