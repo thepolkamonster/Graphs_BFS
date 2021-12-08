6b4561ae8223f21fc8ee125d9710515018cb09d3
thepolkamonster <72528293+thepolkamonster@users.noreply.github.com>
2021-12-08 08:13:16 +0530

    Update BFS.cpp

--- BFS.cpp
+++ BFS.cpp
@@ -1,15 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+void BFS(int g[][7],int start,int n)
+{
+    int i=start;
+    queue<int> q;
+    int visit[7]={0};
+
+    cout<<i;
+    visit[i];
+    visit[i]=1;
+    q.push(i);
+    while(!q.empty())
+    {
+        i=q.front();
+        q.pop();
+        for(int j=1;j<n;j++)
+        {
+            if(g[i][j]==1 && visit[j]==0)
+            {
+                cout<<j;
+                visit[j]=1;
+                q.push(j);
+            }
+        }
+    }
+}
 int main()
 {
-    list<int> list1;
-    list<int> list2(7);
-    list1.push_back(5);
-    list1.push_back(7);
-    list<int> :: iterator t;
-    t=list1.begin();
-    cout<<*iter;
-
-
+ int G[7][7]={{0,0,0,0,0,0,0},
+ {0,0,1,1,0,0,0},
+ {0,1,0,0,1,0,0},
+ {0,1,0,0,1,0,0},
+ {0,0,1,1,0,1,1},
+ {0,0,0,0,1,0,0},
+ {0,0,0,0,1,0,0}};
+ BFS(G,1,7);
+ 
+ return 0;
 }
